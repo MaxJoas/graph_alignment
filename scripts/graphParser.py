6dc740ca2b95865652539ace3cf5bfbcdaa785d9
mk36fyvy <mikreck@hotmail.com>
2019-04-17 18:19:11 +0200

    added bronKerbusch.py and graphParser.py in scripts/

--- scripts/graphParser.py
+++ scripts/graphParser.py
@@ -30,6 +30,9 @@ def parseGraph(doc):
                 else:   #if nodes are not labelled
                     nodes[(splitList[0], "")] = splitList[1:]
             
+                
+
+
                 if not checkList[3] and not checkList[4]:
                     createUndirectedEdges(nodes, edges)
                 else:
@@ -48,20 +51,41 @@ def parseGraph(doc):
                         print("Oh no, something went wrong here! File contains too many empty lines.")
                         return
 
+        nodes = makeNodesReal(nodes)
+
     pprint.pprint(checkList)
     pprint.pprint(nodes)
-    pprint.pprint(edges)
+    #pprint.pprint(edges)
     print("Successfully parsed " + sys.argv[1].split("/")[-1])
     return (checkList, nodes, edges)
 
 def createUndirectedEdges(nodes, edges):
     done = []
     for node in nodes:
+
         for neighbour in nodes[node]:
-            if neighbour not in done:
+            #if neighbour not in done:
                 edges[(node, neighbour)] = ["", ""]
-        done.append(node)
+        #done.append(node)
+
+#get neighbours in nodes labels
+def makeNodesReal(nodes): 
+    tupelHelperList = list(nodes.keys()) #list of (id, label) for building dictionary
+    tupelHelperDict = {}        
+    
+
+    for node in tupelHelperList:    
+        tupelHelperDict[node[0]] = node[1] #dictionary which contains (id, label)
+    
+    #changes the neighbours to labelled neighbours for every node in nodes
+    for node in nodes:
+        d = nodes[node]
+        for neighbour in nodes[node]:
+            temp = (neighbour, tupelHelperDict[neighbour])
+            location = nodes[node].index(neighbour)
+            nodes[node][location] = temp
+    return nodes
 
 #EXECUTION ROAD#
 
-parseGraph(sys.argv[1])
+#parseGraph(sys.argv[1])
