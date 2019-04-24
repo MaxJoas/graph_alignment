e53458c914069602a8dd0fe32a2cded73e176665
ClayMannn <s4userclma@gmail.com>
2019-04-24 16:37:25 +0200

    aplha2

--- scripts/mProduct.py
+++ scripts/mProduct.py
@@ -1,6 +1,4 @@
-G= [A1,]
 
-H= [(1, 2), (2, 3), (1, 4), (4, 2)]
 
 #The vertex set of the modular product of G and H is the cartesian product V(G) ×  V(H). Any two vertices (u, v) and (u' , v' ) are adjacent in the modular product of G and H if and only if either
 
@@ -12,10 +10,29 @@ H= [(1, 2), (2, 3), (1, 4), (4, 2)]
 
 #if node in node.neighbours
 
+#    def __init__ ( self, id, label ):
+
+#        self.id = id
+#        self.label = label
+#        self.neighbours = []
+
+#    def add_neighbour(self, node):
+#        self.neighbours.append(node)
+
+
+
 def mProduct(G,H):
 	mP = []
+
+	def __init__ ( mP, id, label ):
+	mP.id = []
+	mP.label = []
+	mP.neighbours = []
+
 	for i in G:
 		for j in H:
 			if (G[i] in H[j].neighbours and  G[i] in H[j].neighbours) or (not G[i] in H[j].neighbours and  not G[i] in H[j].neighbours) :
-				mP.apppend(G[i], H[j])
+				mP.id.apppend(G[i].id + H[j].id)
+				mP.label.apppend(G[i].label, H[j].label)
+				mP.neighbours.apppend(G[i].neighbours, H[j].neighbours)
 	return (mP)
