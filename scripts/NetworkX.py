97d7747251857bae018630eedeaef62a67f08158
ClayMannn <s4userclma@gmail.com>
2019-04-26 20:33:02 +0200

    random Graph (Tree) generator

--- scripts/NetworkX.py
+++ scripts/NetworkX.py
@@ -8,13 +8,20 @@
 import networkx as nx
 import math
 import matplotlib.pyplot as plt
+from networkx.readwrite import json_graph
+import json
 
 G = nx.Graph()
 elist = [(1, 2), (2, 3), (1, 4), (4, 2)]
 G.add_edges_from(elist)
 elist = [('a', 'b', 5.0), ('b', 'c', 3.0), ('a', 'c', 1.0), ('c', 'd', 7.3)]
 G.add_weighted_edges_from(elist)
+#___________________________________________________________________________
 
+
+G = nx.fast_gnp_random_graph(10, 10, seed=None, directed=True) #fast_gnp_random_graph(n, p, seed=None, directed=False)[source]
+
+#G = nx.balanced_tree(3, 3) #balanced_tree(r, h[, create_using]) #nx.random_tree(6[, 1]) #nx.random_tree(n[, seed])
 #___________________________________________________________________
 #funktioniert noch nicht die Darstellung
 #G = nx.cubical_graph()
@@ -23,6 +30,7 @@ nx.draw(G, with_labels = 1) # default spring_layout
 plt.show()
 #plt.subplot(122)
 #nx.draw(G, pos=nx.circular_layout(G), nodecolor='r', edge_color='b') #10.1  Matplotlib
+data = json_graph.tree_data(G,root=1) #only saves directed Tree Graphs :/
 
 #nx.complete _graph(10) #all with all
 #.order() #Kanten
