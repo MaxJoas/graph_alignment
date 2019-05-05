9686011837f84297dd20f6420e23b99a84c46712
Max <maxjoas.mj@gmail.com>
2019-05-05 16:07:22 +0200

    max bk pivot and bk normal

---
+++ scripts/fun_with_classes/bk_pivot.py
@@ -0,0 +1,67 @@
+
+from graph import Graph
+import sys
+import pprint
+import random
+
+''' implementing broknkerbosch algorithmn where r is the list of possible nodes
+in a clique, p is the list of canditates and x is the garbage collection'''
+
+def bk_pivot ( r, p, x ):
+
+    # when p and x are empty return r as max clique
+    if not any ( [p, x] ):
+        pprint.pprint(r)
+        return r
+
+    pivot = random.choice( p + x ) # chosing pivot randomlt from union of p, r
+
+    # loop through canditates p without neighbours of pivot element
+    for v in list( set(p) - set( pivot.neighbours ) ) :
+
+        r_ = r + [v] # concatenate r and v
+
+        # intersection of x respectively p and neighbours of v
+        x_ = [ v for v in v.neighbours if v in x ]
+        p_ = [ v for v in v.neighbours if v in p ]
+
+        bk_pivot ( r_, p_, x_ ) # recursive call of broknkerbosch
+
+        p.remove(v) # taking current node out of canditates
+        x.append(v) # adding current node to garbage collection
+
+def bk ( r, p, x ):
+
+    # when p and x are empty return r as max clique
+    if not any ( [ p, x ] ):
+        pprint.pprint(r)
+        return r
+
+    for v in p[:] :
+
+        r_ = r + [v] # concatenate r and v
+
+        # intersection of x respectively p and neighbours of v
+        x_ = [ v for v in v.neighbours if v in x ]
+        p_ = [ v for v in v.neighbours if v in p ]
+
+        bk_pivot ( r_, p_, x_ ) # recursive call of broknkerbosch
+
+        p.remove(v) # taking current node out of canditates
+        x.append(v) # adding current node to garbage collection
+
+
+# EXECUTION (PIVOT VERSION) ----------------------------------------------------
+
+if __name__ == '__main__':
+
+    try:
+        graph = Graph(sys.argv[1])
+        r = x = []
+        p = graph.nodes
+
+        bk_pivot ( r, p, x )
+
+
+    except:
+        print( ' please provide a graph file as argument \n example: python3 bk_pivot.py graph.graph' )
