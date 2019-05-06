ba755fd93b723470d2260fd23f5a24412b2eec35
Max <maxjoas.mj@gmail.com>
2019-05-06 22:19:24 +0200

    bk pivot fix

--- scripts/fun_with_classes/bk_pivot.py
+++ scripts/fun_with_classes/bk_pivot.py
@@ -14,10 +14,14 @@ def bk_pivot ( r, p, x ):
         pprint.pprint(r)
         return r
 
-    pivot = random.choice( p + x ) # chosing pivot randomly from union of p, r
+    pivot = random.choice( p  + r ) # chosing pivot randomly from union of p, r
+    #pprint.pprint( pivot )
 
     # loop through canditates p without neighbours of pivot element
-    for v in list( set(p) - set( pivot.neighbours ) ) :
+    for v in p[:] :
+
+        if  v in pivot.neighbours:
+            pass
 
         r_ = r + [v] # concatenate r and v
 
@@ -45,7 +49,7 @@ def bk ( r, p, x ):
         x_ = [ v for v in v.neighbours if v in x ]
         p_ = [ v for v in v.neighbours if v in p ]
 
-        bk_pivot ( r_, p_, x_ ) # recursive call of broknkerbosch
+        bk ( r_, p_, x_ ) # recursive call of broknkerbosch
 
         p.remove(v) # taking current node out of canditates
         x.append(v) # adding current node to garbage collection
@@ -56,12 +60,12 @@ def bk ( r, p, x ):
 if __name__ == '__main__':
 
     try:
+
         graph = Graph(sys.argv[1])
         r = x = []
         p = graph.nodes
 
         bk_pivot ( r, p, x )
-
 
     except:
         print( ' please provide a graph file as argument \n example: python3 bk_pivot.py graph.graph' )
