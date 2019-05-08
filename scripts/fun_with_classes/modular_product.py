37154d9af8decac21f41f033c3d2b2fec5695e73
Maximilian Joas <45900229+MaxJoas@users.noreply.github.com>
2019-05-08 22:17:38 +0200

    modularproduct final

--- scripts/fun_with_classes/modular_product.py
+++ scripts/fun_with_classes/modular_product.py
@@ -1,27 +1,32 @@
 
+
 from graph import Graph
 from node import Node
 import sys
 import pprint
 from parser import parse_graph
-from bk_pivot import bk
+from bk_pivot import bk_pivot
 from view_graph import create_graph
 
 
-''' building cartesian product of two graphs '''
+''' building cartesian product of two graphs and making new node objects out
+of the given node objects from each graph. In order do get the neighbour
+relations right we need to save the nodes of the two input graphs to user
+their .neighbour method later'''
 
 def cart_product(G,H):
 
-	# init empty set to store nodes
-	cart_product = []
-	for vertex_g in G:
-		for vertex_h in H:
-			cart_product.append( (vertex_g , vertex_h ) )
+
+	cart_product = {} # empty dicht to store consolidated nodes as keys and tupel of old nodes as values
+	for g in G:
+		for h in H:
+			cur_node = Node( g.id + h.id, g.label + h.label )
+			cart_product[cur_node] = (g,h)
 
 	return cart_product
 
 
-''' this function checks if the two given new nodes ( of the cartesian product)
+''' this function checks if the two given new nodes (of the cartesian product)
 of two graphs are neighbours according to the rules of the modular product which'''
 
 def neighbours_in_mp ( tup1, tup2 ):
@@ -36,35 +41,36 @@ def neighbours_in_mp ( tup1, tup2 ):
 	return False
 
 
-''' this function build the modular product out of the cartesian product therfore
-we take each node (here a Tuple of two nodes) and make a node object of the tupel
-and find neighbours of new nodes accoring to rules of modular prodcut'''
+''' this function builds the modular product out of the cartesian product by
+applying the neighbour rules of the modular product'''
 
 def mod_product( cartp ):
 
 	modular_set = set() # empty set for storing Node objects of modular product
-	for tup in cartp :
+	for n in cartp.keys() :
 
-		#consolidation fo nodes to one new node object
-		cur_node = Node( tup[0].id + tup[1].id, tup[0].label + tup[1].label )
+		for t in cartp.keys() :
 
-		for t in cartp :
+			# prevents that the node (old Tupel nodes) gets compared with itself
+			if cross_compare_tupels ( cartp[n], cartp[t] ):
+				if neighbours_in_mp( cartp[n], cartp[t] ):
 
-			# prevents that the node gets compared with itself
-			if not  (tup[0].id == t[0].id or tup[1].id == t[1].id):
-				if neighbours_in_mp(tup, t):
-					cur_node.add_neighbour(Node( t[0].id + t[1].id, t[0].label + t[1].label))
+					n.add_neighbour(t)
 
-		# add complete node object at the end of first for loop
-		modular_set.add(cur_node)
 
-	# OUTPUT
-	modular_product_as_graph = Graph(modular_set)
+		# add complete consolidated node (with neighbours) at the end of first for loop
+		modular_set.add(n)
 
-	modular_product_as_graph.create_undirected_edges()
-	#print(modular_product_as_graph.nodes)
+	# OUTPUT-------------------------------------------------------------------
+	modular_product_as_graph = Graph(modular_set) # making graph out of modular product nodes
+	modular_product_as_graph.create_undirected_edges() #giving the graph edges
 
 	return modular_product_as_graph
+
+def cross_compare_tupels( tup, t ):
+	if not  (tup[0].id == t[0].id or tup[1].id == t[1].id):
+		return True
+	return False
 
 # EXECUTION  -------------------------------------------------------------------
 
@@ -74,15 +80,13 @@ if __name__ == '__main__':
 		g = parse_graph( sys.argv[1] )
 		h = parse_graph( sys.argv[2] )
 		modp = mod_product( cart_product( g.nodes, h.nodes ) )
-		#print(modp)
-		x = []
-		r = []
+		print(modp)
+		#print(list(modp.nodes)[1].neighbours)
+		x = set()
+		r = set()
 		p = list(modp.nodes)
-		print(p[3])
 
-
-
-		bk( r, p, x)
+		bk_pivot( r, p, x)
 		#create_graph(modp.nodes ,modp.edges)
 
 	except Exception as e:
