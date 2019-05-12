54a095bd4d8819ad7244054dc0d0dfed8d84b9a4
ClayMannn <s4userclma@gmail.com>
2019-05-12 20:51:09 +0200

    version_0_2

--- scripts/fun_with_classes/Cordella.py
+++ scripts/fun_with_classes/Cordella.py
@@ -8,40 +8,40 @@ import sys
 import pprint
 import random
 
-#INPUT: an intermediate state s; the initial state s 0 has M(s 0 )=∅
+#INPUT: an intermediate state intermediate_state; the initial state intermediate_state 0 has M(intermediate_state 0 )=∅
 #OUTPUT: the mappings between the two graphs
+def P(G1, G2, intermediate_state?): 
+#Compute the set P(intermediate_state) of the pairs candidate for inclusion in matchs(intermediate_state)
+
+
+def F(intermediate_state, n, m, G1=n?, G2=m?): #Durchführbarkeit testen? True/False
+#boolean fkt. Machbarkeitsfunktion F will also prune some states that, 
+#albeit corresponding to an isomorphism between G 1 (intermediate_state) and G 2 (intermediate_state), would not lead to a complete matching solution.
+#wenngleich
+  if (corresponding to an isomorphism between G 1 (intermediate_state) and G 2 (intermediate_state) == True):
+    return (True)
 
 
 
-def F(intermediate_state, n, m): #boolean fkt. Machbarkeitsfunktion F will also prune some states that, albeit corresponding to an isomorphism between G 1 (s) and G 2 (s), would not lead to a complete matching solution.
-
-
-
-
-
-
-
-def matchs(intermediate_state, G1, G2):
+def matchs(intermediate_state, G1, G2):#M maps each branch of G 1 onto a branch of G 2 and (vice versa?????
 	while (procedure == True):		
 		if (intermediate_state covers all nodes G2.node):
-				return matchs(intermediate_state) #?
-			else
-			#Compute the set P(s) of the pairs candidate for inclusion in matchs(intermediate_state)
-				for n in G1.node: #FOREACH (n, m)∈ P(s)???
+				return matchs(intermediate_state) #intermediate_state covers all nodes G2.node ??MATCH??
+		else:
+			#P(G1, G2, intermediate_state?) #Compute the set P(intermediate_state) of the pairs candidate for inclusion in matchs(intermediate_state)
+				for n in G1.node: #FOREACH (n, m)∈ P (G1, G2, intermediate_state?): 
 					for m in G2.node:
 						if (F(intermediate_state, n, m) == True ):
-							#Compute the state s ́ obtained by adding (n, m) to M(s)
+							#Compute the state intermediate_state ́ obtained by adding (n, m) to M(intermediate_state)
 							matchs(intermediate_state) #?
-						#end if ?
-				#end for each?
-			#restore data structure?
-		#end if?
-	# end procedure == False ???
+		end if (end for each? == True?)
+      restore data structure?
+		end if (end procedure == False)
 			
 
 
 
 intermediate_state=0 #the_initial_state
-G1=
+G1=			#G 1 = (N 1 , B 1 ) and G 2 = (N 2 , B 2 )
 G2=
 matchs(intermediate_state, G1, G2)
