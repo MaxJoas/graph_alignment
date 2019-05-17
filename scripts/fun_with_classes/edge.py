041a295055d965a9c027d612b6a0f864176481b3
mk36fyvy <mikreck@hotmail.com>
2019-05-17 14:35:29 +0200

    made Node and Edge objects comparable (set.add works now)

--- scripts/fun_with_classes/edge.py
+++ scripts/fun_with_classes/edge.py
@@ -9,6 +9,9 @@ class Edge(object):
         self.node2 = node2
         self.label = label
 
+
+
+
     #checks if edge is reverse (as in a,b to b,a) to another edge 
     def is_reverse_of( self, e2 ):
         if self.node1 == e2.node2 and self.node2 == e2.node1:
@@ -16,9 +19,24 @@ class Edge(object):
         else:
             return False
 
+
+
+    def __eq__( self, other ):
+        return self.node1 == other.node1 and self.node2 == other.node2 and self.label == other.label
+    
+    
+    
+    def __hash__( self ):
+        return hash((self.node1, self.node2, self.label))
+
+
+
     def __str__( self ):
 
         return "\n ({} to {}) '{}'".format( self.node1.id, self.node2.id, self.label )
 
+    
+    
+    
     def __repr__(self):
         return self.__str__()
--- scripts/fun_with_classes/node.py
+++ scripts/fun_with_classes/node.py
@@ -9,15 +9,34 @@ class Node():
         self.label = label
         self.neighbours = set()
 
+
+
     def add_neighbour(self, node):
+
         self.neighbours.add(node)
 
+
+
+
+    def __eq__( self, other ):
+        return self.id == other.id and self.label == other.label and self.neighbours == other.neighbours
+
+
+
+    def __hash__( self ):
+        return hash((self.id, self.label))
+
+
     def __str__( self ):
+
         neighbours_string = " "
+        
         for neighbour in self.neighbours:
             neighbours_string += neighbour.id
             neighbours_string += " "
         return "\n Node " + self.id + " '" + self.label + "' {" + str(neighbours_string) + "}"
+
+
 
     def __repr__(self):
         return self.__str__()
