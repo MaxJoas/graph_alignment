bce45583a1b2cd450166c697baf10240522170b5
MaxJoas <maxjoas.mj@gmail.com>
2019-05-29 20:32:29 +0200

    vf2 80char

--- scripts/fun_with_classes/vf2_beauty.py
+++ scripts/fun_with_classes/vf2_beauty.py
@@ -1,5 +1,8 @@
+import sys
+
 from node import Node
 from graph import Graph
+from parser import parse_graph
 
 
 class VF2():
@@ -17,7 +20,7 @@ class VF2():
             self.type = 'isomorphism'
 
         self.small_g, self.large_g = g, h
-        if g > h:
+        if g.int_size() > h.int_size():
             self.small_g = h
             self.large_g = g
 
@@ -42,32 +45,24 @@ class VF2():
         self.in_s = self.out_s = self.small_g.gen_dict( 0 )
         self.in_l = self.out_l = self.large_g.gen_dict( 0 )
 
-    '''
-    TODO: print command should be in main
-    '''
-    '''main matching function, is called recursively and returns legal matches'''
 
     def match( self, last_mapped=(Node("0", ""), Node("0", "")), depth=0 ):
 
         if self.s_in_small_g():
 
-            print("\nEND_RESULT: \nType: {} \n\n{}\n".format(self.type, self.core_s))
+            print( "\nEND_RESULT: \nType: {} \n\n{}\n".format(self.type, self.core_s ))
             self.restore_ds( last_mapped[0], last_mapped[1], depth )
-
             return self.core_s
 
-        lens = self.set_inout( last_mapped[0], last_mapped[1], depth )
-        p = self.compute_p(lens)
+        td = self.set_inout( last_mapped[0], last_mapped[1], depth )
+        p = self.compute_p(td)
 
         for tup in p:
 
-            if self.is_feasible(tup[0], tup[1], depth, lens):
-
+            if self.is_feasible(tup[0], tup[1], depth, td):
                 self.compute_s_( tup[0], tup[1], depth )
                 self.match( tup, depth+1 )
-
-                #print("\n Call! \n\n last_mapped: {} \n\n lens: {} \n\n depth: {} \n\n core_l: {} \n\n core_s {} \n\n".format( tup, lens, depth, self.core_l, self.core_s ) )
-
+                #print("\n Call! \n\n last_mapped: {} \n\n td: {} \n\n depth: {} \n\n core_l: {} \n\n core_s {} \n\n".format( tup, td, depth, self.core_l, self.core_s ) )
         self.restore_ds( last_mapped[0], last_mapped[1], depth )
 
     def s_in_small_g(self):
@@ -80,16 +75,16 @@ class VF2():
                 return False
         return True
 
-    def compute_p(self, lens):
+    def compute_p(self, td):
 
-        if all( ( lens["out_l"] ,lens["out_s"] ) ):
+        if all( ( td["out_l"] ,td["out_s"] ) ):
             return self.cart_p(self.out_l, self.legal_max(self.out_s))
 
-        elif all(( not lens['in_l'], not lens['in_s'], lens['out_l'], lens['out_s'] )):
+        elif all(( not td['in_l'], not td['in_s'], td['out_l'], td['out_s'] )):
             return self.cart_p(self.in_l,  self.legal_max(self.in_s))
 
-        elif not any((lens["in_l"], lens["in_s"], lens["out_l"], lens["out_s"])):
-
+        elif not any((td["in_l"], td["in_s"], td["out_l"], td["out_s"])):
+            # all mapped nodes are in m_l (large_g) or m_s (small_g)
             m_l = {n for n in self.core_l if self.core_l[n] != self.null_n}
             m_s = {n for n in self.core_s if self.core_s[n] != self.null_n}
             # In diff_l are all nodes that are in the large graph, but not mapping
@@ -98,34 +93,24 @@ class VF2():
             return self.cart_p(diff_l, max(diff_s))
         return set()
 
-    def is_feasible(self, n ,m, depth, lens):
-
-        #print("Tested tup: {}".format(tup))
+    def is_feasible( self, n ,m, depth, td):
         #0-look-ahead
         if not all((
             self.zero_look_ahead(n, m, self.core_l),
-            self.zero_look_ahead(m, n, self.core_s)
-        )):
-
-            #print("0-look-ahead error")
+            self.zero_look_ahead(m, n, self.core_s) ) ):
             return False
 
         if self.type == "isomorphism":
-
             # 1-look-ahead
-            if not (lens["in_l"] == lens["in_s"] or lens["out_l"] == lens["out_s"]):
+            if not ( td["in_l"] == td["in_s"] or td["out_l"] == td["out_s"] ):
                 #print("1-look-ahead error")
                 return False
-
         elif self.type == "subgraph":
-
             # 1-look-ahead
-            if not (lens["in_l"] >= lens["in_s"] or lens["out_l"] >= lens["out_s"]):
-                # print("1-look-ahead-error")
+            if not ( td["in_l"] >= td["in_s"] or td["out_l"] >= td["out_s"] ):
                 return False
 
-        elif not self.two_look_ahead(depth, lens):
-            # print("2-look-ahead-error")
+        elif not self.two_look_ahead(depth, td):
             return False
         return self.check_semantics()
 
@@ -143,20 +128,15 @@ class VF2():
 
         self.core_l[n] = self.null_n
         self.core_s[m] = self.null_n
-
         #print("Depth: {} \n Restored tup: {}".format(depth, last_mapped))
 
-# HELPER FUNCTIONS -------------------------------------------------------------
-
-    '''
-    Saves number of nodes for each terminal set in lens and sets ssr /
-    recursion depth, if not set. Used for computing candidate set p.
-    '''
-
+# HELPER FUNCTIONS ------------------------------------------------------------
     def set_inout(self, n, m, depth):
-
-        lens = {"in_l": 0, "out_l": 0, "in_s": 0, "out_s": 0}
-
+        '''
+        Saves number of nodes for each terminal set in td and sets ssr /
+        recursion depth, if not set. Used for computing candidate set p.
+        '''
+        td = {"in_l": 0, "out_l": 0, "in_s": 0, "out_s": 0}
         # makes sure, the first selected node gets depth
         try:  # This is needed, because the very first try will fail
             if self.in_l[n] == 0 and self.out_l[n] == 0:
@@ -166,47 +146,42 @@ class VF2():
             if self.in_s[m] == 0 and self.out_s[m] == 0:
                 self.in_s[m] = depth
                 self.out_s[m] = depth
-
         except:
             pass
 
         # Compute terminal_dicts and length for the large graph
         for v in n.neighbours:
-
             if not self.core_l[v] == self.null_n :
                 continue
 
             if v in n.in_neighbours:
                 # saves current depth for terminal node in case it hasn't depth yet
-                lens[ 'in_l' ] += 1
+                td['in_l'] += 1
                 if self.in_l[v] == 0: self.in_l[v] = depth
 
             if v in n.out_neighbours :
-                lens[ 'out_l' ] += 1
+                td['out_l'] += 1
                 if self.out_l[v] == 0: self.out_l[v] = depth
-
 
         # compute terminal_dicts for the small graph
         for v in m.neighbours:
-
             if not self.core_s[v] == self.null_n:
                 continue
 
             if v in m.in_neighbours:
                 if self.in_s[v] == 0: self.in_s[v] = depth
-                lens[ 'in_s' ] += 1
+                td['in_s'] += 1
 
             if v in m.out_neighbours:
-                lens[ 'out_s' ] += 1
+                td['out_s'] += 1
                 if self.out_s[v] == 0: self.out_s[v] = depth
-        return lens
+        return td
 
-    '''creates the cartesian product of every large_g node with max(id) node in small_g'''
-
-    def cart_p(self, t_dict, t_max):
+    def cart_p( self, node_dict, t_max ):
+        """creates the cartesian product """
         cp = set()
-        for node in t_dict:
-            cp.add((node, t_max))
+        for node in node_dict:
+            cp.add( (node, t_max) )
         return cp
 
     def legal_max(self, t_dict):
@@ -219,11 +194,8 @@ class VF2():
                 max_node = node
         return max_node
 
-
     def zero_look_ahead( self, n, m, core ):
-        '''every neighbour of n ( tested tup: (n,m) ) has to be mapped to a
-        neighbour of m'''
-
+        '''every neighbour of n has to be mapped to a neighbour of m'''
         for n_ in n.neighbours:
             m_ = core[n_] # Mapping of v
             if m_ == self.null_n : # If mapping does'n exist take next neighbour
@@ -241,14 +213,12 @@ class VF2():
                 return False
         return True
 
-    def two_look_ahead(self, depth, lens):
-
-        free_n1 = len(self.large_g.nodes) - depth - lens["in_l"] - lens["out_l"]
-        free_n2 = len(self.small_g.nodes) - depth - lens["in_s"] - lens["out_s"]
+    def two_look_ahead(self, depth, td):
+        free_n1 = len(self.large_g.nodes) - depth - td["in_l"] - td["out_l"]
+        free_n2 = len(self.small_g.nodes) - depth - td["in_s"] - td["out_s"]
 
         if self.type == "isomorphism":
             return free_n1 == free_n2
-
         elif self.type == "subgraph":
             return free_n1 >= free_n2
 
@@ -256,7 +226,6 @@ class VF2():
         return True
 
     def restore_terminals(self, t_dict, dict_key, core, depth):
-
         for node in t_dict:
             if core[node] == self.null_n and t_dict[node] == depth:
                 t_dict[node] = 0
@@ -264,11 +233,7 @@ class VF2():
 
 if __name__ == "__main__":
 
-    from parser import parse_graph
-    import sys
-
     large_g = parse_graph(sys.argv[1])
     small_g = parse_graph(sys.argv[2])
-
     vf2 = VF2(large_g, small_g)
     vf2.match()
