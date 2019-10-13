fcec34efde45db977ed160bef7f0a118d16a1251
ClayMannn <46049671+ClayMannn@users.noreply.github.com>
2019-10-13 16:11:47 +0200

    working smile_parser

--- scripts/fun_with_classes/smiles_parser.py
+++ scripts/fun_with_classes/smiles_parser.py
@@ -2,102 +2,92 @@ from parser import parse_graph
 import sys
 from pysmiles import read_smiles
 import networkx as nx
+#import numpy as np
+
+smiles = 'c1cccc(OC(=O)C)c1C(=O)O'
+mol = read_smiles(smiles, explicit_hydrogen=False, zero_order_bonds=True, reinterpret_aromatic=True)
+# explicit_hydrogen determines whether hydrogen atoms should be represented as explicit nodes in the created molecule, or implicit in the 'hcount' attribute.
+#  zero_order_bonds determines whether zero-order bonds (.) in the SMILES string should result in edges in the produced molecule.
+#    reinterpret_aromatic determines whether aromaticity should be reinterpreted, and determined from the constructed molecule, or whether the aromaticity specifications from the SMILES string (lower case elements) should be taken as leading. If True, will also set bond orders to 1 for bonds that are not part of an aromatic ring and have a bond order of 1.5. If False, will create a molecule using only the information in the SMILES string.
+
+#print(mol.nodes(data='element'))
+#print(mol.nodes(data='aromatic'))
+#print(mol.nodes(data='order'))
 
 
-smiles = 'C1CC[13CH2]CC1C1CCCCC1'
-mol = read_smiles(smiles)
+#print(is_directedmol.edges)
+#print(nx.adjacency_matrix(mol, weight='order').todense())
+#[[0.  1.5 0.  0.  0.  0.  0.  0.  0.  1.5 0.  0.  0. ]
+# [1.5 0.  1.5 0.  0.  0.  0.  0.  0.  0.  0.  0.  0. ]
+# [0.  1.5 0.  1.5 0.  0.  0.  0.  0.  0.  0.  0.  0. ]
+# [0.  0.  1.5 0.  1.5 0.  0.  0.  0.  0.  0.  0.  0. ]
+# [0.  0.  0.  1.5 0.  1.  0.  0.  0.  1.5 0.  0.  0. ]
+# [0.  0.  0.  0.  1.  0.  1.  0.  0.  0.  0.  0.  0. ]
+# [0.  0.  0.  0.  0.  1.  0.  2.  1.  0.  0.  0.  0. ]
+# [0.  0.  0.  0.  0.  0.  2.  0.  0.  0.  0.  0.  0. ]
+# [0.  0.  0.  0.  0.  0.  1.  0.  0.  0.  0.  0.  0. ]
+# [1.5 0.  0.  0.  1.5 0.  0.  0.  0.  0.  1.  0.  0. ]
+# [0.  0.  0.  0.  0.  0.  0.  0.  0.  1.  0.  2.  1. ]
+# [0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  2.  0.  0. ]
+# [0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  1.  0.  0. ]]
+#0;C
 
 
-print(mol.nodes(data='element'))
-# [(0, 'C'),
-#  (1, 'C'),
-#  (2, 'C'),
-#  (3, 'C'),
-
-print(mol.nodes(data='hcount'))
-# [(0, 2),
-#  (1, 2),
-#  (2, 2),
-#  (3, 2),
+#mol_with_H = read_smiles(smiles, explicit_hydrogen=True)
+#print(mol_with_H.nodes(data='element'))
 
 
-mol_with_H = read_smiles(smiles, explicit_hydrogen=True)
-print(mol_with_H.nodes(data='element'))
-# [(0, 'C'),
-#  (1, 'C'),
-#  (2, 'C'),
-#  (3, 'C'),
+#G=nx.path_graph(4)
+#nx.write_adjlist(G, nx.adjacency_matrix(mol, weight='order').todense())
+#AttributeError: 'matrix' object has no attribute 'write'
 
-
-def g_writer(mol):
+def s_writer(mol):
 
     z = "y" #input("do you want to save the plot? (y=yes):\n")
 
     if z=="y":
-        f = open('Graphwirter.graph', 'w')
-        f.write("AUTHOR: Clemens M., Max. J, Michel K., NetworkX\n")
-        f.write("#nodes;" + str(len(mol.nodes)))
+        f = open('smile.graph', 'w')
+        f.write("AUTHOR: Clemens M., Max. J, Michel K., NetworkX, pysmiles, ")
+        f.write(str(smiles))
+        f.write("\n#nodes;" + str(len(mol.nodes)))
         f.write("\n#edges;"+ str(len(mol.edges)))
-        f.write("\nNodes labelled;"+ str(mol.nodes_are_labelled))
-        f.write("\nEdges labelled;"+ str(mol.edges_are_labelled))
-        f.write("\nDirected graph;"+ str(mol.is_directed))
+        f.write("\nNodes labelled;"+ str("True"))
+        f.write("\nEdges labelled;"+ str("False"))
+        f.write("\nDirected graph;"+ str("False"))
         f.write("\n\n")
-        if mol.nodes_are_labelled == "False":
-            for i in (mol.nodes):
-                split_list = i.split(" ")
-                f.write(split_list[1])
-                f.write(";\n")
+        for i in (mol.nodes(data='element')):
+            _list=(str(i))
+            _list = _list.replace("'", "")
+            _list = _list.replace("(", "")
+            _list = _list.replace(")", "")
+            _list = _list.replace(",", ";")
+            _list = _list.replace(" ", "")
+            f.write(_list)
+            #f.write(";")
+            #f.write(split_list[3])
             f.write("\n")
-        else:
-            for i in (mol.nodes):
-                _list=(str(i))
-                _list = _list.replace("'", "")
-                split_list = _list.split(" ")
-                f.write(split_list[2])
-                f.write(";")
-                f.write(split_list[3])
-                f.write("\n")
+            #print(_list)
+        f.write("\n")
+        
+        for i in (mol.edges):
+            _list=(str(i))
+            _list = _list.replace("'", "")
+            _list = _list.replace("(", "")
+            _list = _list.replace(")", "")
+            _list = _list.replace(",", ";")
+            _list = _list.replace(" ", "")
+            f.write(_list)
+            #f.write(";")
+            #f.write(split_list[3])
             f.write("\n")
+            #print(_list)
+        f.write("\n")
+        
 
-        if not mol.is_directed:
-            for edge1 in mol.edges:
-                reverse=False
-                for edge2 in mol.edges:
-                    if edge1.is_reverse_of(edge2):
-                        reverse=True
-                if reverse==False:
-                    print(edge1)
-                    _list=(str(edge1))
-                    _list = _list.replace("'", "")
-                    _list = _list.replace("(", "")
-                    _list = _list.replace(")", "")
-                    _list = _list.replace(" ", "")
-                    _list = _list.replace("to", ";")
-                    split_list = _list.split(" ")
-                    f.write(_list)
-        else:
-            for edge1 in mol.edges:
-                _list=(str(edge1))
-                _list = _list.replace("'", "")
-                _list = _list.replace("(", "")
-                _list = _list.replace(")", "")
-                _list = _list.replace(" ", "")
-                _list = _list.replace("to", ";")
-                split_list = _list.split(" ")
-                f.write(_list)
-    f.close()
-    print("\nthe graph is saved as Graphwirter.graph")
 
-        #print("split_list:" + str(split_list))
 
 
 # EXECUTION  -------------------------------------------------------------------
 
 if __name__ == '__main__':
-    try:
-        mol = parse_graph( sys.argv[1] )
-        g_writer(mol)
-    except Exception as e:
-        print(e)
-        print("please provide the graph you want to test the graphwriter with \n example: python3 graph_writer.py /home/clm/graph_alignment/graphs/graph1.graph")
-#python3 graph_writer.py /home/clm/graph_alignment/graphs/graph1.graph
+    s_writer(mol)
