0845f01b9390f70b8c17336c38647593112eed58
Michel <mikreck@hotmail.com>
2019-11-16 18:32:36 +0100

    multivitamin new

---
+++ multivitamin/multivitamin/basic/graph.py
@@ -0,0 +1,139 @@
+import sys
+
+from multivitamin.basic.node import Node
+from multivitamin.basic.edge import Edge
+
+
+class Graph():
+
+    def __init__(
+        self,
+        id=None,
+        nodes=None,
+        edges=None,
+        nodes_labelled=None,
+        edges_labelled=None,
+        is_directed=None
+    ):
+
+        self.id = id if id else ""
+        self.nodes = nodes if nodes else set()
+        self.edges = edges if edges else set()
+        self.nodes_are_labelled = nodes_labelled if nodes_labelled else False
+        self.edges_are_labelled = edges_labelled if edges_labelled else False
+        self.is_directed = is_directed if is_directed else False
+        
+        self.newick = id
+        self.abbrev = ""
+
+
+    def set_id( self, id ):
+        self.id = id
+
+
+    def get_graph( self, id ):
+        if self.id == id:
+            return self
+
+
+    def get_inout_neighbours( self ):
+        for cur_node in self.nodes:
+            for cur_edge in self.edges:
+
+                if cur_node == cur_edge.node1:
+                    cur_node.out_neighbours.add( cur_edge.node2 )
+
+                if cur_node == cur_edge.node2:
+                    cur_node.in_neighbours.add( cur_edge.node1 )
+
+
+    def create_undirected_edges( self ):
+        done = [] #already checked nodes, used to avoid including reverse edges
+        
+        for node in self.nodes:
+            for neighbour in node.neighbours:
+                if not neighbour in done: #done is for undirected #no bijection
+                    cur_edge = Edge(node, neighbour)
+                    self.edges.add(cur_edge)
+        
+            done.append(node)
+
+
+    def create_fake_directions( self ):
+        if self.is_directed:
+            print( "Fake directions should only be created for undirected graphs!" )
+            return False
+        else:
+            for edge in list(self.edges)[:]:
+                rev_edge = Edge( edge.node2, edge.node1, edge.label )
+                self.edges.add(rev_edge)
+
+
+    '''allow comparing graphs (number of nodes!) to each other ( ==, >=, <=, <, > )'''
+    def __eq__( self, other):
+        if not isinstance(other, Graph):
+            return NotImplemented
+        else:
+            return self.id == other.id
+            # return len(self.nodes) == len(other.nodes)
+
+    def __gt__( self, other ):
+        if not isinstance(other, Graph):
+            return NotImplemented
+        else:
+            if len(self.nodes) == len(other.nodes):
+                return len(self.edges) > len(other.edges)
+            return len(self.nodes) > len(other.nodes)
+
+    def __lt__( self, other ):
+        if not isinstance(other, Graph):
+            return NotImplemented
+        else:
+            if len(self.nodes) == len(other.nodes):
+                return len(self.edges) < len(other.edges)
+            return len(self.nodes) < len(other.nodes)
+
+    def __ge__( self, other ):
+        if not isinstance(other, Graph):
+            return NotImplemented
+        else:
+            if len(self.nodes) == len(other.nodes):
+                return len(self.edges) >= len(other.edges)
+            return len(self.nodes) >= len(other.nodes)
+
+    def __le__( self, other ):
+        if not isinstance(other, Graph):
+            return NotImplemented
+        else:
+            if len(self.nodes) == len(other.nodes):
+                return len(self.edges) <= len(other.edges)
+            return len(self.nodes) <= len(other.nodes)
+
+
+    '''define the way a graph is printed'''
+    def __str__( self ):
+        p_nodes = ""
+        for node in self.nodes:
+            p_nodes += str(node) + "\n"
+        
+        p_edges = ""
+        for edge in self.edges:
+            p_edges += str(edge) + "\n"
+
+        return "{} ;\n {} ;\n {};\n Nodes labelled? {}\n Edges labelled? {}\n Directed graph? {}".format(self.id, self.nodes, self.edges, self.nodes_are_labelled, self.edges_are_labelled, self.is_directed)
+
+        # return str(self.id) # for debugging purposes
+
+    def __repr__( self ):
+        return self.__str__()
+
+
+    def int_size ( self ):
+        return len(self.nodes)
+
+
+    def gen_dict( self,  value ):
+        _dict = {}
+        for key in self.nodes:
+            _dict[key] = value
+        return _dict
---
+++ multivitamin/multivitamin/multiVitamin.py
@@ -0,0 +1,153 @@
+import os
+import pprint
+
+from multivitamin.custom import get_results_dir
+from multivitamin.basic.graph import Graph
+from multivitamin.utils.parser import parse_graph
+from multivitamin.utils.guide_tree import Guide_tree
+from multivitamin.utils.flags import parser
+from multivitamin.utils.graph_writer import write_graph, write_shorter_graph
+from multivitamin.utils.modular_product_class import MP
+from multivitamin.supp.view_graph import create_graph
+from multivitamin.algorithms.bk_pivot_class import BK
+from multivitamin.algorithms.vf2_beauty import VF2
+
+''' 
+FLAGS:
+-a BK VF2 
+-c use algorithm for single alignment and save co-optimals
+-g guide
+-m save in list as input graphs
+-n save_guide
+-s save_all
+-v view
+'''
+
+args = parser.parse_args()
+args.algorithm = args.algorithm.upper()
+
+def main():
+
+    # print(type(args.files[0]))
+
+             
+    print("                                                              ")
+    print("                 _ _   _       _  _                   _       ")
+    print("                | | | (_)     (_)| |                 (_)      ")
+    print(" _ __ ___  _   _| | |_ \ \   / / | |_  __ _ _ __ ___  _ _ __  ")
+    print("| '_ ` _ \| | | | | __| \ \ / / ||  _|/ _` | '_ ` _ \| | '_ \ ")
+    print("| | | | | | |_| | | |_| |\ V /| || |_| (_| | | | | | | | | | |")
+    print("|_| |_| |_|\__,_|_|\__|_| \_/ |_| \__|\__,_|_| |_| |_|_|_| |_|")
+    print("                                                              ")
+    print("                                                  v1.0.0      ")
+    print("                                                              ")                                            
+
+
+
+    if args.files:
+        if isinstance(args.files[0], list): #this happens when parsing files from a directory
+            graphs = args.files[0] 
+        else:
+            graphs = args.files
+
+        guide_tree = Guide_tree( graphs, args.algorithm, args.save_all )
+        print("Calculating multiple alignment with {} algorithm...".format( args.algorithm ))
+        guide_tree.upgma()
+        save_results( guide_tree )
+
+    elif args.coopt:
+        if isinstance(args.coopt[0], list): #this happens when parsing files from a directory
+            graphs = args.coopt[0] 
+        else:
+            graphs = args.coopt
+        
+        if not len(graphs) == 2:
+            raise Exception("You must provide exactly 2 graph files with '-c' ! Use '-m' if you want to align multiple graphs.")
+
+        fake_tree = Guide_tree( graphs, args.algorithm, False )
+        print("Calculating alignment using {} algorithm...".format( args.algorithm ))
+
+        if args.algorithm == "BK":
+            mp = MP( graphs[0], graphs[1] )
+            bk = BK( mp.g, mp.h )
+            x = set()
+            r = set()
+            p = list(mp.modp)
+            bk.bk_pivot( r, p, x )
+            res = bk.get_coopt()
+            res = bk.clique_to_node_set()
+            temp = Graph("")
+            counter = 1
+            for node_set in res:
+                temp = fake_tree.make_graph_real( Graph( "{}--{}#{}".format(graphs[0].id, graphs[1].id, counter), node_set) )
+                # print(temp)
+                fake_tree.intermediates.append( temp )
+                counter += 1
+            save_results( fake_tree )
+
+        elif args.algorithm == "VF2":
+            vf2 = VF2( graphs[0], graphs[1] )
+            vf2.match()
+            for result_graph in vf2.result_graphs:
+                result_graph.create_undirected_edges()
+                fake_tree.intermediates.append( result_graph )
+            save_results( fake_tree )
+
+        else:
+            raise Exception("Invalid algorithm name!")
+
+    elif args.view:
+        # print(args.view)
+        if isinstance(args.view, list): #this happens when parsing files from a directory
+            print("Displaying {}...".format(args.view[0].id))
+            create_graph( args.view[0].id, args.view[0].nodes, args.view[0].edges )
+        else:
+            print("Displaying {}...".format(args.view.id))
+            create_graph( args.view.id, args.view.nodes, args.view.edges )
+        
+
+    else:
+        raise Exception("No graph was parsed from the command-line")
+
+
+def save_results( guide_tree ):
+    path = get_results_dir()
+    
+    # create results directory
+    if not os.path.isdir("{}/{}".format( os.getcwd(), path )): # if results/ does not exist
+        try:
+            os.mkdir("{}{}".format( os.getcwd(), path ) ) 
+        except:
+            print ("Creation of the directory %s failed" % path)
+        else:
+            print ("Successfully created the directory {}{}\n All files will be saved here.".format( os.getcwd(), path ))
+    else:
+        print("\nAll files will be saved in {}{} \n".format( os.getcwd(), path ))
+
+    # save all intermediate alignment graphs, if flag is set
+    if args.save_all or args.coopt:
+        for graph in guide_tree.intermediates:
+            write_graph( graph, path )
+    else:
+        write_graph( guide_tree.result, path )
+
+    # save end alignment graph with much shorter node ids
+    if args.save_shorter:
+        write_shorter_graph( guide_tree.result, path )
+
+    # save graph abbreviations used for identifying original nodes in node ids
+    f = open("{}{}/{}".format( os.getcwd(), path, "graph_abbreviations.txt" ), 'w+')
+    for abbrev, id in guide_tree.graph_abbreviations.items():
+        f.write("{}\t{}\n".format( abbrev, id))
+    f.close
+
+    print("")
+    print("Saved graph id abbreviations as graph_abbreviations.txt")
+
+    # save newick tree in easily parseable txt file
+    if args.save_guide:
+        f = open("{}{}/{}".format( os.getcwd(), path, "newick.txt" ), 'w+')
+        f.write("{}\n".format(guide_tree.newick))
+        f.close
+
+        print("Saved the alignment tree in Newick format as newick.txt\n")
