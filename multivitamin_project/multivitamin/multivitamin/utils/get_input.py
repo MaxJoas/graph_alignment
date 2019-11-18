bb91adf5d0aa96187824608a597f4eba6c610ec1
mk36fyvy <mikreck@hotmail.com>
2019-11-18 22:40:00 +0100

    readding multivitamin

---
+++ multivitamin_project/multivitamin/multivitamin/utils/get_input.py
@@ -0,0 +1,26 @@
+import os
+
+from multivitamin.utils.parser import parse_graph
+
+
+def process_file( filename ):
+    if os.path.isdir(filename):
+        print("parsing .graph files in the given directory\n")
+        return process_directory(filename)
+    else:
+        if not filename.endswith('.graph'):
+            raise Exception("{} is not a valid input (not a .graph file)!".format(filename))
+        return parse_graph( filename ) # this is what is done to the parsed command-line arguments (graphs)
+
+
+def process_directory( directory_name ):
+    graphs = []
+    for filename in os.listdir(directory_name)[:]:
+        # print(filename)
+        if filename.endswith('.graph'):
+            graphs.append(parse_graph((filename)))
+            # print(graphs)
+    if not graphs:
+        raise Exception("No .graph file found in the given directory! Aborting...")
+
+    return graphs
---
+++ multivitamin_project/multivitamin/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='multivitamin',
+    version='1.0',
+    packages=[
+        'multivitamin',
+        'multivitamin.algorithms',
+        'multivitamin.basic',
+        'multivitamin.supp',
+        'multivitamin.utils'
+    ],
+    install_requires=[
+        'matplotlib>=3.1.1',
+        'networkx>=2.4'
+    ],
+    entry_points={
+        'console_scripts': ['multiVitamin=multivitamin.multiVitamin:main']
+    }
+)
